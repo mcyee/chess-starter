da544d35d215cb8daf4ecceb210c909a09c094cd
Christopher Shankland <shankland@bigvikinggames.com>
2014-03-04 10:02:51 -0500

    Now with pieces

--- chess.js
+++ chess.js
@@ -1,6 +1,33 @@
 // Game configuration contstants
 var BOARD_WIDTH = 960;
 var BOARD_HEIGHT = 640;
+var MARGIN = 20;
+var SQUARE_SIZE = ~~((BOARD_HEIGHT - (2 * MARGIN)) / 8);
+var WHITE_SQUARE_STYLE = "#FFFFFF";
+var BLACK_SQUARE_STYLE = "#000000";
+var BORDER_STYLE = "#3404A1";
+var PIECE_STYLE = "#20A020";
+var PIECE_SIZE = 50;
+
+// Unicode piece strings
+var PIECES = {
+	"white": {
+		"king": "\u2654",
+		"queen": "\u2655",
+		"rook": "\u2656",
+		"bishop": "\u2657",
+		"knight": "\u2658",
+		"pawn": "\u2659"
+	},
+	"black": {
+		"king": "\u265A",
+		"queen": "\u265B",
+		"rook": "\u265C",
+		"bishop": "\u265D",
+		"knight": "\u265E",
+		"pawn": "\u265F"
+	}
+};
 
 /**
  * Chess class
@@ -30,6 +57,91 @@ Chess.prototype = {
 		this.__fps = 0;
 		this.__ticks = 0;
 		this.__lastTime = 0;
+
+		this.__createBoard();
+
+	},
+
+	/**
+	 * Create the board data structure
+	 *
+	 * @private
+	 */
+	__createBoard: function __createBoard() {
+
+		var squares = {};
+
+		for (var y = 0; y < 8; ++y) {
+
+			squares[y] = {};
+			for (var x = 0; x < 8; ++x) {
+
+				var piece = "";
+
+				switch (y) {
+					case 0:
+						switch (x) {
+							case 0:
+							case 7:
+								piece = PIECES.black.rook;
+								break;
+							case 1:
+							case 6:
+								piece = PIECES.black.knight;
+								break;
+							case 2:
+							case 5:
+								piece = PIECES.black.bishop;
+								break;
+							case 3:
+								piece = PIECES.black.queen;
+								break;
+							case 4: 
+								piece = PIECES.black.king;
+								break;
+						}
+						break;
+					case 1:
+						piece = PIECES.black.pawn;
+						break;
+					case 6:
+						piece = PIECES.white.pawn;
+						break;
+					case 7:
+						switch (x) {
+							case 0:
+							case 7:
+								piece = PIECES.white.rook;
+								break;
+							case 1:
+							case 6:
+								piece = PIECES.white.knight;
+								break;
+							case 2:
+							case 5:
+								piece = PIECES.white.bishop;
+								break;
+							case 3:
+								piece = PIECES.white.queen;
+								break;
+							case 4: 
+								piece = PIECES.white.king;
+								break;
+						}
+						break;
+				}
+				
+				squares[y][x] = {
+					y: y,
+					x: x,
+					piece: piece,
+					fill: (x + y) % 2 === 0 ? WHITE_SQUARE_STYLE : BLACK_SQUARE_STYLE
+				};
+
+			}
+		}
+
+		this.__board = squares;
 
 	},
 
@@ -71,6 +183,17 @@ Chess.prototype = {
 	},
 
 	/**
+	 * Update callback
+	 *
+	 * @param {Number} now - Absolute time stamp (ms)
+	 * @param {Number} dt - Time since last draw (ms)
+	 * @public
+	 */
+	update: function update(now, dt) {
+
+	},
+
+	/**
 	 * Draw callback
 	 *
 	 * @param {Number} now - Absolute time stamp (ms)
@@ -86,16 +209,7 @@ Chess.prototype = {
 		this.__updateFPSCounter(now, dt);
 		this.__drawFPSCounter(ctx);
 
-	},
-
-	/**
-	 * Update callback
-	 *
-	 * @param {Number} now - Absolute time stamp (ms)
-	 * @param {Number} dt - Time since last draw (ms)
-	 * @public
-	 */
-	update: function update(now, dt) {
+		this.__drawBoard(ctx);
 
 	},
 
@@ -131,15 +245,58 @@ Chess.prototype = {
 	/**
 	 * Draw the FPS counter
 	 *
-	 * @param {Number} now - Absolute time stamp (ms)
-	 * @param {Number} dt - Time since last draw (ms)
-	 * @public
+	 * @param {CanvasRenderingContext2D} ctx
+	 * @private
 	 */
 	__drawFPSCounter: function __drawFPSCounter(ctx) {
 
 		ctx.font = "15px calibri";
 		ctx.fillStyle = "#FF0000";
 		ctx.fillText("FPS: " + ~~this.__fps, 5, 17);
+
+	},
+
+	/**
+	 * Draw the board
+	 * 
+	 * @param {CanvasRenderingContext2D} ctx
+	 * @private
+	 */
+	__drawBoard: function __drawBoard(ctx) {
+
+		ctx.translate(0.5 * (BOARD_WIDTH - (8 * SQUARE_SIZE)), MARGIN);
+
+		// Draw a border
+		ctx.fillStyle = BORDER_STYLE;
+		ctx.fillRect(~~(-MARGIN * 0.5), ~~(-MARGIN * 0.5),
+					 ~~(SQUARE_SIZE * 8 + MARGIN), ~~(SQUARE_SIZE * 8 + MARGIN));
+
+		var board = this.__board;
+		for (var x in board) {
+			for (var y in board[x]) {
+				var square = board[x][y];
+
+				var dx = (square.x) * SQUARE_SIZE;
+				var dy = (square.y) * SQUARE_SIZE;
+
+				ctx.translate(dx, dy);
+				ctx.fillStyle = square.fill;
+				ctx.fillRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
+
+				if (square.piece !== "") {
+					ctx.fillStyle = PIECE_STYLE;
+					ctx.font = PIECE_SIZE + "px calibri";
+					var pieceTextMetrics = ctx.measureText(square.piece);
+					ctx.translate(~~(0.5 * (SQUARE_SIZE - pieceTextMetrics.width)), ~~(0.5 * (SQUARE_SIZE - PIECE_SIZE)));
+					ctx.fillText(square.piece, 0, PIECE_SIZE);
+					ctx.translate(~~(-0.5 * (SQUARE_SIZE - pieceTextMetrics.width)), ~~(-0.5 * (SQUARE_SIZE - PIECE_SIZE)));
+				}
+
+				ctx.translate(-dx, -dy);
+			}
+		}
+
+		ctx.translate(-(0.5 * (BOARD_WIDTH - (8 * SQUARE_SIZE))), -MARGIN);
 
 	}
 
